887d0d2215fda5cf68d1b7e5ac82acb60b38a6b7
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2024-09-26 00:25:59 +0900

    Swiper導入してちょっと見た目整えた
    Co-authored-by: Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>

--- kaizenFitnessForiPhone/kaizenFitnessForiPhone/Components/CardComponent.swift
+++ kaizenFitnessForiPhone/kaizenFitnessForiPhone/Components/CardComponent.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct CardComponent: View {
+    var menu: String
+    var mile: String
+    var minute: String
+    var point: String
+
+
     var body: some View {
         VStack {
             VStack {
@@ -24,28 +30,41 @@ struct CardComponent: View {
                     .padding(.bottom, 10)
             }
             .background(Color(red: 1, green: 0.37, blue: 0.23, opacity: 0.7))
-            VStack {
-                Text("Relax")
+            .clipShape(.rect(
+                topLeadingRadius: 16,
+                bottomLeadingRadius: 0,
+                bottomTrailingRadius: 0,
+                topTrailingRadius: 16
+            ))
+
+
+
+            VStack(spacing: 10) {
+                Text(self.menu)
+                    .bold()
                 Image(systemName: "figure.yoga")
-                HStack {
-                    VStack {
+                HStack(spacing: 25) {
+                    VStack(spacing: 5) {
                         Image(systemName: "mappin.circle")
-                        Text("9.32")
+                        Text(self.mile)
+                            .bold()
                         Text("miles")
                     }
-                    VStack {
+                    VStack(spacing: 5) {
                         Image(systemName: "clock.arrow.trianglehead.counterclockwise.rotate.90")
-                        Text("14")
+                        Text(self.minute)
+                            .bold()
                         Text("minutes")
                     }
-                    VStack {
+                    VStack(spacing: 5) {
                         Image(systemName: "heart.fill")
-                        Text("30")
+                        Text(self.point)
+                            .bold()
                         Text("point")
                     }
                 }
                 Button {
-                    
+
                 } label: {
                     HStack {
                         Image(systemName: "paperplane")
@@ -54,7 +73,7 @@ struct CardComponent: View {
                     .primaryButtonStyle()
                 }
                 Button {
-                    
+
                 } label: {
                     HStack {
                         Text("View detail")
@@ -73,5 +92,5 @@ struct CardComponent: View {
 }
 
 #Preview {
-    CardComponent()
+    CardComponent(menu: "Relax", mile: "9.32", minute: "14", point: "30")
 }
--- kaizenFitnessForiPhone/kaizenFitnessForiPhone/PlanView.swift
+++ kaizenFitnessForiPhone/kaizenFitnessForiPhone/PlanView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct PlanView: View {
+    @State var currentPage: Int = 1
+
+    let totalPages = 2
+
     var body: some View {
         VStack {
             HStack {
@@ -23,10 +27,31 @@ struct PlanView: View {
                     .frame(alignment: .leading)
                     .padding(.bottom, 30)
                     .foregroundColor(Color(red: 1, green: 0.37, blue: 0.23, opacity: 1))
-                
+
                 Spacer()
             }
-            CardComponent()
+            TabView(selection: $currentPage) {
+                CardComponent(menu: "Relax", mile: "9.32", minute: "14", point: "30")
+                CardComponent(menu: "Relax", mile: "9.32", minute: "14", point: "30")
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .gesture(DragGesture().onEnded({ value in
+                if value.translation.width < 0 && currentPage < totalPages {
+                    currentPage += 1
+                } else if value.translation.width > 0 && currentPage > 1 {
+                    currentPage -= 1
+                }
+            }))
+
+            HStack(spacing: 8) {
+                ForEach(1...totalPages, id: \.self) { page in
+                    Circle()
+                        .fill(page == self.currentPage ? Color.blue : Color.gray.opacity(0.3))
+                        .frame(width: 8, height: 8)
+                }
+            }
+            .padding(.vertical, 16)
+            .frame(maxWidth: .infinity, alignment: .bottom)
         }
     }
 }
